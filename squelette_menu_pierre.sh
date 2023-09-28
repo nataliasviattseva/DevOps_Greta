f9f6a6d5c063e8406a300077580abd1f2aa2c42b
nataliasviattseva <30287691+nataliasviattseva@users.noreply.github.com>
2023-09-28 14:45:59 +0200

    Create squelette_menu_pierre.sh

---
+++ squelette_menu_pierre.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#----APPEL FONCTIONS EXTERNES----
+. fonctions.sh
+
+no_color
+
+PS3="
+ Action : (1-Infos) (2- Convertir IP) (3-Ajout IP) (4-Ajout Profil) (5-Scan IP)>
+ select reponse in infos convert ajout profil scan web dhcp cve retour
+ do
+ case $reponse in x y ...
+ 
+x )
+clear
+;;
+
+
+y )
+clear
+;;
+
+
+x )
+clear
+;;
+
+
+x )
+clear
+;;
+
+
+x )
+clear
+;;
+
+
+x )
+clear
+;;
+
+
+retour )
+clear
+red
+echo TRAITEMENT TERMINE
+echo
+no_color
+break
+;;
+ 
+ 
+ esac
+ 
+ done
