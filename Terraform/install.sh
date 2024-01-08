74cc28996dc71704295171f04d8a899027d151e6
nataliasviattseva <30287691+nataliasviattseva@users.noreply.github.com>
2024-01-08 15:14:16 +0100

    Create install.sh

---
+++ Terraform/install.sh
@@ -0,0 +1,36 @@
+# Installation d'Ansible
+echo "Ansible installation"
+apt-get install -y ansible
+clear
+
+# Installation de Vagrant
+echo "Vagrant installation"
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+apt-get update && apt-get install -y vagrant
+clear
+
+# Installation de Terraform
+# Ensure that your system is up to date and you have installed the gnupg, software-properties-common, and curl packages installed. You will use these packages to verify HashiCorp's GPG signature and install HashiCorp's Debian package repository.
+apt-get update && apt-get install -y gnupg software-properties-common
+
+# Install the HashiCorp GPG key.
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# Add the official HashiCorp repository to your system. The lsb_release -cs command finds the distribution release codename for your current system, such as buster, groovy, or sid.
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+tee /etc/apt/sources.list.d/hashicorp.list
+
+# Download the package information from HashiCorp.
+sudo apt update
+
+# Install Terraform from the new repository.
+apt-get install -y terraform
+clear
+
+# Parcer installation
+echo "Parcer installation"
+sudo apt-get install -y packer
