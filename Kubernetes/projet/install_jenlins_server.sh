78885bb6ab092018cc7bf66326d626dbf99ac2b8
nataliasviattseva <30287691+nataliasviattseva@users.noreply.github.com>
2024-01-24 12:46:42 +0100

    Update and rename install.sh to install_jenlins_server.sh

--- Kubernetes/projet/install_jenlins_server.sh
+++ Kubernetes/projet/install_jenlins_server.sh
@@ -22,3 +22,10 @@ sudo chmod 777 /var/run/docker.sock
 
 # Installing Sonarqube on Jenkins Server
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+
+# Installing the Trivy tool on the Jenkins Server
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
